f0012ac314b30217e79c2c4725baf3634a8bd083
Ankit Patidar <122157990+Ankitpatidar4@users.noreply.github.com>
2026-07-09 16:44:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -16,15 +18,21 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
+if (!process.env.MONGO_URI) {
+  console.log("MONGO_URI missing in environment variables");
+  process.exit(1);
+}
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.log("MongoDB connection error:", err);
+    process.exit(1);
   });
