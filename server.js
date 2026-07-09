853a95d84b434e20aa12acdec7138117d063ba89
Ankit Patidar <122157990+Ankitpatidar4@users.noreply.github.com>
2026-07-09 17:09:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-const HOST = "0.0.0.0";
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -17,10 +16,14 @@ app.get("/", (req, res) => {
   res.status(200).send("Unity Game API Running");
 });
 
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
+});
+
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 mongoose
