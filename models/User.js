3e3d89019ae42d438591c03b433ccfff25692bc7
Ankit Patidar <122157990+Ankitpatidar4@users.noreply.github.com>
2026-07-10 18:43:21 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,48 +1,78 @@
 const mongoose = require("mongoose");
 
+const appRewardSchema = new mongoose.Schema(
+  {
+    appName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    kcEarned: {
+      type: Number,
+      default: 100
+    },
+
+    linkedAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    _id: false
+  }
+);
+
 const userSchema = new mongoose.Schema(
-{
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
     },
 
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true
     },
 
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
 
     deviceId: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
 
     appNames: {
-        type: [String],
-        default: []
+      type: [String],
+      default: []
+    },
+
+    appRewards: {
+      type: [appRewardSchema],
+      default: []
     },
 
     kc: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
+
     avatar: {
-    type: Number,
-    default: 0
-}
-},
-{
+      type: Number,
+      default: 0
+    }
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
